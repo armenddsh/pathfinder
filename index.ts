588957612405368fd0a99fd171f7677dea684e28
Armend Shala <armenddsh@gmail.com>
2021-01-19 16:18:32 +0100

    Add method for displaying spots in the area

--- index.ts
+++ index.ts
@@ -1,15 +1,31 @@
-import PathFinder, { Area, Position, Spots } from "./pathFinder";
+import PathFinder, { Area, GridArea, Position, Spots } from "./pathFinder";
 
 
-const pf = new PathFinder({ width: 1, height: 10 });
+const displaySpots = function(gridArea: GridArea) {
+    for (let x = 0; x < gridArea.length; x++) {
+        let text = "";
+        for (let y = 0; y < gridArea[x].length; y++) {
+            const cell = gridArea[x][y];
+            if (cell.w) {
+                text += " X ";
+            } else {
+                text += " . ";
+            }
+        }
+        console.log(text);
+    }
+};
+
+const pf = new PathFinder({ width: 10, height: 10 });
 let gridArea = pf.createArea();
 
 const start: Position = { x: 0, y: 0 };
-const end: Position = { x: 10, y: 10 };
-const area: Area = { width: 300, height: 100 };
+const size: Area = { width: 2, height: 2 };
 const spots: Spots = { openList: [], closedList: [] };
 
-const path = pf.findPath( start, end, spots );
-const areaWithSpots = pf.addSpotsToArea(start, area, spots);
+let areaWithSpots;
+areaWithSpots = pf.addSpotsToArea(start, size, gridArea);
+areaWithSpots = pf.addSpotsToArea({ x: 4, y: 4 }, { width: 2, height: 2 }, gridArea);
+areaWithSpots = pf.addSpotsToArea({ x: 8, y: 8 }, { width: 2, height: 2 }, gridArea);
 
-console.log(areaWithSpots);
+displaySpots(areaWithSpots);
--- pathFinder.ts
+++ pathFinder.ts
@@ -6,13 +6,13 @@ export type Position = { x: number; y: number };
 export type Start = Position;
 export type End = Position;
 export type Cell = { x: number; y: number; z: number, h: number };
-export type Spot = { x: number; y: number; z: number; };
+export type Spot = { x: number; y: number; w: boolean; };
 
 export default class PathFinder {
     constructor(public area: Area) { }
 
     public findPath(start: Start, end: End, spots: Spots): Position[] {
-        spots.openList.push({ x: start.x, y: start.y, z: 0 });
+        spots.openList.push({ x: start.x, y: start.y, w: false });
 
         return [];
     }
@@ -27,23 +27,23 @@ export default class PathFinder {
 
         for (let x = 0; x < gridArea.height; x++) {
             for (let y = 0; y < gridArea.width; y++) {
-                grid[x][y] = { x, y, z: 0 };   
+                grid[x][y] = { x, y, z: false };   
             }
         }
 
         return grid;
     }
 
-    public addSpotsToArea(positionStart: Position, area: Area, spots: Spots): Spots {
+    public addSpotsToArea(positionStart: Position, area: Area, gridArea: GridArea): GridArea {
         for (let x = 0; x < area.width; x++) {
             for (let y = 0; y < area.height; y++) {
                 const positionX = positionStart.x + x;
                 const positionY = positionStart.y + y;
 
-                spots.closedList.push({ x: positionX, y: positionY, z: 0 });   
+                gridArea[positionX][positionY].w = true;    
             }
         }
         
-        return spots;
+        return gridArea;
     }
 }
